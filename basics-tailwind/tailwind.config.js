3545344ccf585950aa4e7d0bb606133677d0397e
shovonroy173 <shovonroy2003@gmail.com>
2024-01-26 11:36:25 +0530

    initial commit

---
+++ basics-tailwind/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/assets/index.css",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
